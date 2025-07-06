9ab2bd447502b1e4e7e25831d8da4a3115071d5d
Ali-Mubeen-Siddiqui <alimubeensiddiqui@gmail.com>
2025-07-06 13:29:00 +0530

    improved CardCheck.java

--- CardCheck.java
+++ CardCheck.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class CardCheck{
     public static void main(String[] args){
+        
+        final int MIN_AGE = 18;
+        final int MIN_CREDIT_SCORE = 600;
+        final double MIN_INCOME = 20000.00;
         Scanner sc = new Scanner(System.in);
         System.out.println("\tWelcome to check you eligibility for credit card");
         
@@ -15,18 +19,18 @@ public class CardCheck{
         System.out.println("Please share your credit score: ");
         int credit_score = sc.nextInt();
         
-        String response = isEligible(age, income, credit_score);
+        String response = isEligible(age, income, credit_score,MIN_AGE,MIN_INCOME,MIN_CREDIT_SCORE);
         System.out.println("\n" + response);
         sc.close();
     
     }    
     
-    public static String isEligible(int age,double income ,int credit_score){
-        if (age<18 || income<20000 || credit_score<600){
-            return "Sorry, you do not qualify for a credit card.";
+    public static String isEligible(int age,double income ,int credit_score, int MIN_AGE,double MIN_INCOME,int MIN_CREDIT_SCORE){
+        if (age>=MIN_AGE && income>=MIN_INCOME && credit_score>=MIN_CREDIT_SCORE){
+            return "Congratulations! You are eligible for a credit card";
         }
         else{
-        return "Congratulations! You are eligible for a credit card";
+        return "Sorry, you do not qualify for a credit card";
         }
     }
 }
